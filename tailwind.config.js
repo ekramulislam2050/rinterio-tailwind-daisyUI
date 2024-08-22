e13a80edb3c08456d7a161c58d300f1ba9384e70
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-08-22 09:12:40 +0600

    added google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        manrope:["Manrope"]
+      },
+    },
   },
   plugins: [
     require('daisyui'),
