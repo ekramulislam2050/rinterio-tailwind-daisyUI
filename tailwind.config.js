a8f080c7cb795724b4b9d960f1e6470ae37fc574
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-08-26 21:27:20 +0600

    added challenges section

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,14 @@ module.exports = {
            card_3:["rgb(164, 218, 195)"],
            card_4:['rgb(119, 170, 234)'],
            footerBg:['rgb(28, 28, 28)'],
-           coraption:['rgb(231, 229, 230)']
+           corruption:['rgb(231, 229, 230)'],
+           accordion_1:['rgb(183, 48, 220)'],
+           accordion_2:['rgb(164, 89, 237)'],
+           accordion_3:['rgb(77, 69, 229)']
       },
       backgroundImage:{
-        my_img:'url(./assets/bg.jpg)'
+        my_img:'url(./assets/bg.jpg)',
+        ph_logo:'url(./assets/ph-logo.png)'
       },
     },
   },
